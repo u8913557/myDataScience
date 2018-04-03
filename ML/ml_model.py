8f156d81673c423e37fe9ed4d0e3735f8844dfb2
u8913557 <u8913557@gmail.com>
2018-04-03 12:04:05 +0800

    First ML implementation
    Perceptron

---
+++ ML/ml_model.py
@@ -0,0 +1,131 @@
+
+import numpy as np
+
+class ml_model(object):
+    """
+    Parameters
+    ------------
+    method : str
+        which ML lib
+    eta : float (default: 0.01)
+        Learning rate (between 0.0 and 1.0)
+    num_epochs : int (default: 100)
+        How many run to train dataset.
+    shuffle : bool (default: True)
+        Shuffles training data every epoch
+
+    Attributes
+    ------------
+    w_ : 1d-array, float
+        Weights
+    errors_ : list, float
+    """
+
+    def __init__(self, method=None, eta=0.01, num_epochs=100, shuffle=None, activation=None):
+        self.method = method
+        self.eta = eta
+        self.num_epochs = num_epochs
+        self.shuffle = shuffle
+        self.activation = activation
+        self.w_ = []
+        self.b_ = 0    
+        self.costs = []    
+        #print("ml_model: __init__")
+
+
+    def fit(self):
+        pass        
+
+    def activation_fn(self, z):
+            return np.piecewise(z, [z < 0, z >= 0], [0, 1])
+
+    def predict(self):
+        pass
+
+    def r2_evl(self, Y, Y_hat):
+        Y = Y.reshape(Y.shape[0], 1)
+        d1 = Y - Y_hat
+        d2 = Y - Y.mean()
+        print("Shape of d1:", d1.shape)
+        print("Shape of d2:", d2.shape)
+        r2 = 1 - (d1.T.dot(d1) / d2.T.dot(d2))
+        return r2
+    
+
+class perceptron(ml_model):
+  
+    def __init__(self, method=None, eta=0.01, num_epochs=100, shuffle=True, activation='step'):
+        #print("perceptron: __init__")  
+        super().__init__(method, eta, num_epochs, shuffle, activation)          
+
+    """
+    Parameters
+    ------------
+    X_train : array, float
+        Dataset for traninig
+    Y : array, float
+        "True" Y
+    """
+    def fit(self, X_train, Y):
+        # print("perceptron: train")
+        #super().train()
+        if (self.shuffle==True):
+            #init w_ as random numbers
+            self.w_ = np.random.randn(1+X_train.shape[1],1)
+
+            #shuffle X_train, Y
+            r = np.random.permutation(Y.shape[0])
+            X_train = X_train[r]
+            Y = Y[r]
+            Y = Y.reshape(Y.shape[0], 1)
+        else:
+            # init w_ as zeros with w0
+            self.w_ = np.zeros(1 + X_train.shape[1], 1)        
+
+        # Add one bias term
+        ones = np.ones((X_train.shape[0], 1))
+        X_train = np.concatenate((ones, X_train), axis=1)
+        
+        print("shape of X_train:", X_train.shape)
+        print("shape of w_:", self.w_.shape)
+        print("shape of Y:", Y.shape)
+        
+        error=np.zeros(self.w_.shape)
+        for epoch in range(self.num_epochs):
+            Y_hat = self.predict(X_train)
+            error = Y - Y_hat
+            
+            if(np.count_nonzero(error, axis=0)==0):
+                print("Finish Training at {0}-th run".format(epoch))
+                break
+            
+            delta_w = self.eta * np.dot(X_train.T, error)
+            self.w_ += delta_w
+            #self.errors_.append(error)
+            
+            if (epoch % 10 == 0):
+                pass
+                #print("{0} th run:".format(i))
+                #print("w_:", self.w_)
+                #print("errors_:\n", self.errors_) 
+        
+        print("final w:", self.w_)
+
+    def net_input(self, X_data):
+        # net input: w0x0 + w1x1... + wixi
+        #print("net_input:")
+        #print("shape of X_data:", X_data.shape)
+        #print("shape of w_:", self.w_.shape)
+        return np.dot(X_data, self.w_)
+
+    def predict(self, z):
+        if (self.activation):
+            return self.activation_fn(self.net_input(z))
+        else:      
+            return input(z)
+
+        
+
+
+
+
---
+++ ML/perceptron_test.py
@@ -0,0 +1,34 @@
+import ml_model
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    # AND Gate
+    X = np.array([
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]
+        ])
+
+    Y = np.array([0, 0, 0, 1])
+
+    plt.scatter(X[Y==1, 0]==1, X[Y==1, 1]==1, color='red', \
+            marker='o', label='Y=1')
+    plt.scatter(X[Y==0, 0]==1, X[Y==0, 1]==1, color='blue', \
+            marker='x', label='Y=0')
+    plt.show()
+
+    perceptron_test = ml_model.perceptron(num_epochs=200)
+    perceptron_test.fit(X, Y)
+
+    ones = np.ones((X.shape[0], 1))
+    X_test = np.concatenate((ones, X), axis=1)
+    Y_hat = perceptron_test.predict(X_test)
+    print("R2:", perceptron_test.r2_evl(Y, Y_hat))
+
+
+
+
+
