4b71c4ef0e88fda5f1ffd0a5e8ee2f12eb9d95d4
u8913557 <u8913557@gmail.com>
2018-05-06 12:30:27 +0800

    Add Cov2D function

--- myUtility.py
+++ myUtility.py
@@ -71,3 +71,27 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                         marker='o',
                         s=100,
                         label='test set')
+
+
+def conv2d(X, W, p=(0, 0), s=(1, 1)):
+    W_rot = np.array(W)[::-1, ::-1]
+    X_orig = np.array(X)
+    n1 = X_orig.shape[0] + 2*p[0]
+    n2 = X_orig.shape[1] + 2*p[1]
+    X_padded = np.zeros(shape=(n1, n2))
+    X_padded[p[0]:p[0]+X_orig.shape[0],
+             p[1]:p[1]+X_orig.shape[1]] = X_orig
+
+    res = []
+    for i in range(0, int((X_padded.shape[0] - \
+        W_rot.shape[0])/s[0])+1, s[0]):
+        
+        res.append([])
+        for j in range(0, int((X_padded.shape[1] - \
+                       W_rot.shape[1])/s[1])+1, s[1]):
+
+            X_sub = X_padded[i:i+W_rot.shape[0],
+                             j:j+W_rot.shape[1]]
+            res[-1].append(np.sum(X_sub * W_rot))
+
+    return(np.array(res))
