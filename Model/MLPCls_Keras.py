3fd1bf5a60562d7836b531c45655a221fad7cc58
u8913557 <u8913557@gmail.com>
2018-05-13 22:42:43 +0800

    Add MLP test for Titanic3 and IMDb

--- Model/MLPCls_Keras.py
+++ Model/MLPCls_Keras.py
@@ -1,6 +1,5 @@
-from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.utils import np_utils
@@ -17,9 +16,13 @@ sys.path.insert(0, abs_file_path)
 from myUtility import show_train_history
 from myUtility import plot_images_labels_prediction
 
+
+
 # data_set = 'iris'
 # data_set = 'mnist'
-data_set = 'cifar10'
+# data_set = 'cifar10'
+# data_set = 'titanic3'
+data_set = 'IMDb'
 
 model_path = os.path.join(cur_path, "SaveModel\\MLP\\Keras\\" + data_set + ".h5")
 
@@ -31,7 +34,10 @@ try:
 except:
     print("No old model data, create new one")
 
+
 if data_set == 'iris':
+    
+    from sklearn import datasets
 
     iris = datasets.load_iris()
     X = iris.data
@@ -39,7 +45,7 @@ if data_set == 'iris':
 
     X_train, X_test, Y_train, Y_test = \
         train_test_split(X, Y, test_size=0.3, random_state=1)
-    
+  
     """
     sc = StandardScaler()
     sc.fit(X)
@@ -183,6 +189,157 @@ elif data_set == 'cifar10':
     model.compile(loss='categorical_crossentropy', metrics=['accuracy'],
                   optimizer=optimizer_)
 
+elif data_set == 'titanic3':
+
+    cur_path = os.path.dirname(__file__)
+    rel_path = "..\\"
+    abs_file_path = os.path.join(cur_path, rel_path)
+    sys.path.insert(0, abs_file_path)
+    from myDataset import get_Titanic
+
+    titanic_df = get_Titanic()
+
+    columns = ['survived', 'name', 'pclass', 'sex', 'age',
+                'sibsp', 'parch', 'fare','embarked']
+    titanic_df_filtered = titanic_df[columns]
+
+    age_mean = titanic_df_filtered['age'].mean()
+    titanic_df_filtered['age'] = titanic_df_filtered['age'].fillna(age_mean)
+    
+    fare_mean = titanic_df_filtered['fare'].mean()
+    titanic_df_filtered['fare'] = titanic_df_filtered['fare'].fillna(fare_mean)
+
+    print('Null data summary:', titanic_df_filtered.isnull().sum())
+
+    titanic_df_filtered['sex'] = titanic_df_filtered['sex'].map({'female':0, 'male':1}).astype(int)
+    titanic_df_processed = pd.get_dummies(data=titanic_df_filtered, columns=['sex', 'embarked'])
+    
+    titanic_data_processed = titanic_df_processed.as_matrix()
+    
+    print("titanic_data_processed:", titanic_data_processed)
+
+    X = titanic_data_processed[:, 2:]
+    Y = titanic_data_processed[:, 0]
+
+    minmax_scale = preprocessing.MinMaxScaler(feature_range=(0, 1))
+    X_processed = minmax_scale.fit_transform(X)    
+
+    print("shape of X_processed:", X_processed.shape)
+    print("shape of Y:", Y.shape)
+    
+    X_train_processed, X_test_processed, Y_train, Y_test = \
+        train_test_split(X_processed, Y, test_size=0.3, random_state=1)
+
+    label_dict={0:"dead", 1:"survived"}
+
+    classes = 2
+    input_size = 10
+    hidden1_neurons = 40
+    hidden2_neurons = 30
+
+    activation_h = 'relu'
+    # activation_h = 'sigmoid'
+    #activation_o = 'softmax'
+    activation_o = 'sigmoid'
+    # optimizer_ = 'sgd'
+    # optimizer_ = 'adam'
+    optimizer_='adadelta'
+    batch_size = 20
+    # batch_size = None
+    epochs = 50
+    valid_data_rate = 0.1
+
+    model.add(Dense(hidden1_neurons, input_dim=input_size, 
+                    kernel_initializer='uniform', activation=activation_h))
+    # model.add(Dropout(0.5))
+    model.add(Dense(hidden2_neurons, kernel_initializer='uniform', 
+                    activation=activation_h))
+    # model.add(Dropout(0.5))
+    model.add(Dense(classes, kernel_initializer='uniform', 
+                    activation=activation_o))
+    model.compile(loss='binary_crossentropy', metrics=['accuracy'],
+                  optimizer=optimizer_)
+
+
+elif data_set == 'IMDb':
+
+    from keras.preprocessing import sequence
+    from keras.preprocessing.text import Tokenizer
+    from keras.layers.embeddings import Embedding
+    from keras.layers import Flatten
+
+    cur_path = os.path.dirname(__file__)
+    rel_path = "..\\"
+    abs_file_path = os.path.join(cur_path, rel_path)
+    sys.path.insert(0, abs_file_path)
+    from myDataset import get_IMDb
+    from myUtility import text_preprocessor
+
+    imdb_df = get_IMDb()
+    imdb_df['review'] = imdb_df['review'].apply(text_preprocessor)
+    
+    imdb_data = imdb_df.as_matrix()
+    
+    X = imdb_data[:, 0]
+    Y = imdb_data[:, 1]
+
+    X_train, X_test, Y_train, Y_test = \
+        train_test_split(X, Y, test_size=0.3, random_state=1)
+        
+    # print('X_train:', X_train)
+
+    max_words_dict = 5000
+    max_words_len = 500
+    text_token = Tokenizer(num_words=max_words_dict)
+    text_token.fit_on_texts(X_train)
+
+    # print("text_token.document_count:", text_token.document_count)    
+    # print("text_token.word_index:", text_token.word_index)
+    # print("text_token.word_docs:", text_token.word_docs)
+
+    X_train_seq = text_token.texts_to_sequences(X_train)
+    X_test_seq = text_token.texts_to_sequences(X_test)
+
+    # print('X_train[0]:', X_train[0])
+    # print('X_train_seq[0]:', X_train_seq[0])
+
+    X_train_processed = sequence.pad_sequences(X_train_seq, maxlen=max_words_len)
+    X_test_processed = sequence.pad_sequences(X_test_seq, maxlen=max_words_len)
+
+    classes = 2
+    input_size = max_words_dict
+    hidden1_neurons = 256
+    # hidden2_neurons = 512
+
+    activation_h = 'relu'
+    # activation_h = 'sigmoid'
+    #activation_o = 'softmax'
+    activation_o = 'sigmoid'
+    # optimizer_ = 'sgd'
+    # optimizer_ = 'adam'
+    optimizer_='adadelta'
+    batch_size = 100
+    # batch_size = None
+    epochs = 50
+    valid_data_rate = 0.2
+
+    embedding_output = 32
+
+    model.add(Embedding(input_dim=input_size, input_length=max_words_len,
+                        output_dim=embedding_output))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(hidden1_neurons, kernel_initializer='normal', 
+                    activation=activation_h))
+    model.add(Dropout(0.5))
+    # model.add(Dense(hidden2_neurons, kernel_initializer='uniform', 
+                    # activation=activation_h))
+    # model.add(Dropout(0.5))
+    model.add(Dense(classes, kernel_initializer='normal', 
+                    activation=activation_o))
+    model.compile(loss='binary_crossentropy', metrics=['accuracy'],
+                  optimizer=optimizer_)
+
 
 Y_train_oneHot = np_utils.to_categorical(Y_train, classes)
 Y_test_oneHot = np_utils.to_categorical(Y_test, classes)
@@ -210,7 +367,7 @@ print(train_history.history.keys())
 
 show_train_history(train_history, valid_data_rate=valid_data_rate)
 
-predict_prob = model.predict(X_test_processed)
+predict_prob = model.predict(X_test_processed)   
 
-if data_set != 'iris':
+if data_set is 'mnist' or data_set is 'cifar10':
     plot_images_labels_prediction(X_test_img, Y_test, predict_result, predict_prob, label_dict, idx=0)
